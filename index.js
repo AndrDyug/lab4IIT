fe6835a583dd0af1c08874fd316c651e068a3f94
Andriy Dyug <37251165+AndrDyug@users.noreply.github.com>
2022-06-19 15:44:32 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const { Telegraf } = require('telegraf')
 
 const bot = new Telegraf("5316113628:AAF01LocCIt3-em6cA-RNgwGc2bLxQcqLRI")
 bot.start((ctx) => ctx.reply('start'))
-bot.hears(`hi`, (ctx) => ctx.reply(`...`))
+bot.hears(`hello`, (ctx) => ctx.reply(`...`))
 bot.launch()
 
 process.once('SIGINT', () => bot.stop('SIGINT'))
